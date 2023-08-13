6463ef741060f3f0be21369385a45f5e1111dd2f
TheBiggestTrees <141491446+TheBiggestTrees@users.noreply.github.com>
2023-08-12 20:54:57 -0500

    Outcomes added
    Created all possible outcomes from the game.

--- script.js
+++ script.js
@@ -14,7 +14,23 @@ function playRound(playerSelection, computer) {
 
     if (playerSelection.toLowerCase() === computer.toLowerCase() ) {
         console.log(`You tied ${playerSelection.toLowerCase()} to ${computer.toLowerCase()}`)
+    } else if (playerSelection.toLowerCase() === "rock" && computer.toLowerCase() === "paper") {
+        console.log('You lost! Paper beats rock.')
+    } else if (playerSelection.toLowerCase() === "rock" && computer.toLowerCase() === "scissors") {
+        console.log('You won! Rock beats scissors.')
+    } else if (playerSelection.toLowerCase() === "paper" && computer.toLowerCase() === "scissors") {
+        console.log('You lost! Scissors beats paper.')
+    } else if (playerSelection.toLowerCase() === "paper" && computer.toLowerCase() === "rock") {
+        console.log('You won! Paper beats rock.')
+    } else if (playerSelection.toLowerCase() === "scissors" && computer.toLowerCase() === "rock") {
+        console.log('You lost! Rock beats scissors.')
+    } else if (playerSelection.toLowerCase() === "scissors" && computer.toLowerCase() === "paper") {
+        console.log('You won! Scissors beats paper.')
     }
+
+
+
+
 
 }
 
