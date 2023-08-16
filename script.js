4d07057e14418a4cf89db49a5b4d5c89f29126e2
TheBiggestTrees <141491446+TheBiggestTrees@users.noreply.github.com>
2023-08-16 16:28:25 -0500

    Added text to page
    removed console.log and instead opted for text on the page

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let round = 1;
 let playerScore = 0;
 let computerScore = 0;
 
+const myScore = document.createElement('div');
+    container.appendChild(myScore);
+
+const enemyScore = document.createElement('div');
+    container.appendChild(enemyScore);
+
+const div = document.createElement('div');
+    container.appendChild(div);
+
 function getComputerChoice() { 
     let computer = choices[(Math.floor(Math.random() * choices.length))];
     return computer;
@@ -11,52 +20,51 @@ function getComputerChoice() {
 function playRound(playerSelection, computer) {
 
     if (playerSelection.toLowerCase() === computer.toLowerCase() ) {
-        console.log(`You tied ${playerSelection.toLowerCase()} to ${computer.toLowerCase()}`);
+        div.textContent = `You tied ${playerSelection.toLowerCase()} to ${computer.toLowerCase()}`;
     } else if (playerSelection.toLowerCase() === "rock" && computer.toLowerCase() === "paper") {
-        console.log('You lost! Paper beats rock.');
+        div.textContent = 'You lost! Paper beats rock.';
         computerScore = ++computerScore;
     } else if (playerSelection.toLowerCase() === "rock" && computer.toLowerCase() === "scissors") {
-        console.log('You won! Rock beats scissors.');
+        div.textContent = 'You won! Rock beats scissors.';
         playerScore = ++playerScore;
     } else if (playerSelection.toLowerCase() === "paper" && computer.toLowerCase() === "scissors") {
-        console.log('You lost! Scissors beats paper.');
+        div.textContent = 'You lost! Scissors beats paper.';
         computerScore = ++computerScore;
     } else if (playerSelection.toLowerCase() === "paper" && computer.toLowerCase() === "rock") {
-        console.log('You won! Paper beats rock.');
+        div.textContent = 'You won! Paper beats rock.';
         playerScore = ++playerScore;
     } else if (playerSelection.toLowerCase() === "scissors" && computer.toLowerCase() === "rock") {
-        console.log('You lost! Rock beats scissors.');
+        div.textContent = 'You lost! Rock beats scissors.';
         computerScore = ++computerScore;
     } else if (playerSelection.toLowerCase() === "scissors" && computer.toLowerCase() === "paper") {
-        console.log('You won! Scissors beats paper.');
+        div.textContent = 'You won! Scissors beats paper.';
         playerScore = ++playerScore;
     }
 }
 
 function rock() {
-        playerSelection = choices[0]
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log("Player Score: " + +playerScore);
-        console.log("AI Score: " + +computerScore);
+    playerSelection = choices[0]
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    myScore.textContent = "Player Score: " + +playerScore;
+    enemyScore.textContent = "AI Score: " + +computerScore;
 }
 
 function paper() {
     playerSelection = choices[1]
     let computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
-    console.log("Player Score: " + +playerScore);
-    console.log("AI Score: " + +computerScore);
+    myScore.textContent = "Player Score: " + +playerScore;
+    enemyScore.textContent = "AI Score: " + +computerScore;
 }
 
 function scissors() {
     playerSelection = choices[2]
     let computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
-    console.log("Player Score: " + +playerScore);
-    console.log("AI Score: " + +computerScore);
+    myScore.textContent = "Player Score: " + +playerScore;
+    enemyScore.textContent = "AI Score: " + +computerScore;
 }
-
 
 function checkWin() {
     if (playerScore === computerScore) {
